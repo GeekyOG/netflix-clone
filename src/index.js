055a1ba8bae2c4131a538a1bdf2d9951a146c7bb
oghomena <onojuvwevwog@gmail.com>
2022-03-04 20:35:44 +0100

    make it better

---
+++ src/index.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import {render} from 'react-dom';
+import './index.css';
+import App from './App';
+import {GlobalStyles} from './globalStyles';
+import { firebase } from './lib/firebase.prod';
+import {FirebaseContext} from './context/firebase'
+
+render(
+  <>
+  <FirebaseContext.Provider value={{firebase}}>
+  <GlobalStyles />
+  <App />
+  </FirebaseContext.Provider>
+  </>,
+  document.getElementById('root')
+);
